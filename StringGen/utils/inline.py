c0a64a02a081cb6ca40550418e3b07e732d5ee0a
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-03-23 16:31:23 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="sᴏᴜʀᴄᴇ", url="https://github.com/Payal8800/String_Session_Gen"
             ),
         ],
     ]
